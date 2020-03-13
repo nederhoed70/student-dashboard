881d5e5322242c8a571eb4f9e7888b499be0b0dd
Jeroen Nederhoed <jeroen@nederhoed.net>
2020-03-13 18:06:14 +0100

    Updates

---
+++ precache-manifest.3642784081d8ab22c89e08ff7fa021a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43d87ff1d886b56fd82f51ba026e2ff9",
+    "url": "/student-dashboard/index.html"
+  },
+  {
+    "revision": "799dcc20d3496cf9376d",
+    "url": "/student-dashboard/static/css/main.9a46ed73.chunk.css"
+  },
+  {
+    "revision": "febe7be531f89aa91167",
+    "url": "/student-dashboard/static/js/2.ec9f5524.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/student-dashboard/static/js/2.ec9f5524.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "799dcc20d3496cf9376d",
+    "url": "/student-dashboard/static/js/main.372a3d30.chunk.js"
+  },
+  {
+    "revision": "1b627f7680c485624c2f",
+    "url": "/student-dashboard/static/js/runtime-main.03d4b650.js"
+  }
+]);
