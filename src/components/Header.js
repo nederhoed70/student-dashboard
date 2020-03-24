33527aa6767e15b87ab2e4076418fa69cb04bf98
Jeroen Nederhoed <jeroen@nederhoed.net>
2020-03-24 09:18:11 +0100

    reduced unnecessairy lines of code rewritten some functions

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,18 +1,6 @@
 import React from 'react';
 
 function Header(props) {
-	const showStudentScore = studentId => {
-		let selectedStudent = studentId;
-		let data = props.data.rawData;
-		const scores = [
-			...new Set(data.filter(object => object.name === selectedStudent))
-		];
-		return {
-			name: selectedStudent,
-			scores: scores
-		};
-	};
-
 	const handleMenu = event => {
 		//	let studentScore = showStudentScore();
 		props.selectedStudentFromChild(event.target.title);
